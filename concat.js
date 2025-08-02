decd08be2449d8c5bd1367f46582eb2428031cb8
smmasudrana <smmasudrana62@gmail.com>
2025-08-02 18:18:08 +0600

    add concat

---
+++ concat.js
@@ -0,0 +1,13 @@
+const first ='abid';
+const last = 'hasan';
+const fullname = first+ ' ' +last;
+console.log(fullname);
+
+const first2 ='abid';
+const last2 = 'hasan';
+const fullname2 = first2.concat(' ').concat(last2);
+console.log(fullname2);
+
+
+console.log(last2.includes('n'));
+console.log(last2.includes('x'));
