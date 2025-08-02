d92fc795427e781fe740ba7efbbae6fa93f4e16d
smmasudrana <smmasudrana62@gmail.com>
2025-08-02 16:12:05 +0600

    add some basics string js

---
+++ string.js
@@ -0,0 +1,21 @@
+const country ='Bangladesh';
+const division = 'Rajshahi';
+const district = `Bogura`;             //back tik qoutetion
+const thana = new String('Sherpur');   //special type
+
+console.log(typeof country);
+console.log(thana);
+
+
+const numbers =[54, 62, 98, 69, 66];
+console.log(numbers.length);
+console.log(numbers[1]);
+numbers[1] =10;
+console.log(numbers[1]);
+
+
+const capital ='dhaka';
+console.log(capital.length);   //in string also count word & spaces with (.length)
+console.log(capital[1]);
+// capital[1] =10;  it's dosent work bcz strng is immutable i mean not changable.
+
