ef1e5fe34154b04c2c9a66c52720f55512412a74
smmasudrana <smmasudrana62@gmail.com>
2025-08-03 10:40:11 +0600

    object declaration

---
+++ object.js
@@ -0,0 +1,18 @@
+const age = 21;
+const school ='rigle square';
+const isPassed = true;
+let isDeveloper;
+const subjects =['bangla', 'english', 'math']
+
+
+//object declare-------non premitive
+const subject ={
+    name:'biology',
+    teacher:'pagla sir',
+    examDate:'30 Feb',
+    chapters:['first', 'second', 'third'],
+    name:{
+        name:'final exam',
+        marks:'100',
+    }
+}
